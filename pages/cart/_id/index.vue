c99f04ec7221a6478f8bae278a9e4b227d0aadb7
Tina Hosseinpour <Tinahosseinpour.79@gmail.com>
2022-05-26 01:47:57 -0700

    axios

--- pages/cart/_id/index.vue
+++ pages/cart/_id/index.vue
@@ -1,8 +1,20 @@
 <template>
-  <h1>{{ content.id }}</h1>
+  <div>
+    <div v-for="item in content" :key="item.id">
+      <span style="color:red">Product description:</span>
+      <p>{{item.description}}</p>
+      <hr />
+    </div>
+  </div>
 </template>
 
 
 <script>
-export default {}
+export default {
+  async asyncData(context) {
+    const data = await context.store.dispatch('products/getAllProducts')
+    console.log(data)
+    return { content: data }
+  },
+}
 </script>
